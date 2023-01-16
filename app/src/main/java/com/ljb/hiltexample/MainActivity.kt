2060780c5664b9c05447f1a42f58337ca3184d93
jblee9374 <55440637+wo9374@users.noreply.github.com>
2023-01-16 16:18:25 +0900

    refactor: domain, data 모듈 새로 추가

--- app/src/main/java/com/ljb/hiltexample/MainActivity.kt
+++ app/src/main/java/com/ljb/hiltexample/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.ljb.hiltexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ljb.hiltexample.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
     }
 }
